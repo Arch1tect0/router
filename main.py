4501ec5e3b8ee76289027f01de1f7f5067fa829c
Arch1tect0 <54332189+Arch1tect0@users.noreply.github.com>
2025-07-17 14:19:46 -0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from router import Supervisor
-
-if __name__ == "__main__":
-    supervisor = Supervisor()
-    query = "Marketing: Can you get me the sales data from Jan 1 to Feb 1?"
-    response = supervisor.route_task(query)
-    print("Final Output:", response)
-
